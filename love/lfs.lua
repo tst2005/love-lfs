295c80cd75c6d8bfb30c8920632146b1d7443b11
TsT <tst2005@gmail.com>
2018-08-29 13:48:10 +0200

    initial impl

---
+++ love/lfs.lua
@@ -0,0 +1,105 @@
+local love = {
+	filesystem = require "love.filesystem"
+}
+
+-- love.filesystem.areSymlinksEnabled
+-- love.filesystem.setSymlinksEnabled
+-- love.filesystem.isSymlink
+-- love.filesystem.getRealDirectory
+
+local lfs = {}
+
+-- [x] lfs.attributes (filepath [, request_name | result_table])
+--love.filesystem.getSize ; love.filesystem.getLastModified
+local _info = love.filesystem.getInfo
+function lfs.attributes(path, request_name_or_result_table)
+	local r
+	if type(request_name_or_result_table) == "string" then
+		local loveinfos = _info(path)
+		local filetype = loveinfos.type
+		if filetype == "symlink" then
+			filetype = "link"
+		elseif filetype ~= "file" and filetype ~= "directory" then
+			filetype = "other"
+		end
+		r = {
+			dev = nil,
+			ino = nil,
+			-- LFS : file, directory, link, socket, named pipe, char device, block device or other
+			-- LOVE: file, directory, symlink or other
+			mode = filetype,
+
+			nlink = nil,
+			uid = 0,
+			gid = 0,
+			rdev = nil,
+			acccess = nil,
+			modification = loveinfos.modtime,
+			change = loveinfos.modtime,
+			size = loveinfos.size,
+			permissions = nil,
+			blocks = nil,
+			blksize = nil,
+			
+		}
+	else
+		--r = request_name_or_result_table
+		error("love.lfs.attributes: result_table not-implemented-yet")
+	end
+	return r
+end
+
+-- [ ] lfs.chdir(path)
+-- [ ] lfs.lock_dir(path, [seconds_stale])
+
+-- [x] lfs.currentdir ()
+local pwd = love.filesystem.getWorkingDirectory
+function lfs.currentdir()
+	return pwd()
+end
+
+-- [x] iter, dir_obj = lfs.dir (path)
+local ls = love.filesystem.getDirectoryItems or love.filesystem.enumerate
+local function fs_iter(udata)
+	local n = udata.n or 1
+	local v = udata[n]
+	if v ~= nil then
+		udata.n = n+1
+	end
+	return v
+end
+function lfs.dir(path)
+	return fs_iter, ls(path)
+end
+
+-- [ ] lfs.lock (filehandle, mode[, start[, length]])
+-- [ ] lfs.link (old, new[, symlink])
+
+-- [x] lfs.mkdir (dirname)
+local mkdir = love.filesystem.createDirectory or love.filesystem.mkdir
+function lfs.mkdir(dirname)
+	return mkdir(dirname)
+end
+
+-- [x] lfs.rmdir (dirname)
+local rm = love.filesystem.remove -- remove file or directory
+local isd = love.filesystem.isDirectory
+local isl = love.filesystem.isSymlink
+function lfs.rmdir(dirname)
+	if isd(path) and not isl(path) then
+		return rm(path)
+	end
+	return false
+end
+
+-- [ ] lfs.setmode (file, mode)
+-- [?] lfs.symlinkattributes (filepath [, aname])
+
+-- [?] lfs.touch (filepath [, atime [, mtime]])
+--local open = love.filesystem.newFile("data.txt")
+
+
+-- [ ] lfs.unlock (filehandle[, start[, length]])
+
+lfs._VERSION = "love.lfs 0.1.0"
+return lfs
